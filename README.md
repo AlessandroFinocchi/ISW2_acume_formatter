eb5ce2da4d2e5e6c3e5bbde1a605d2de2681d929
Alessandro <alessandro.finocchi@students.uniroma2.eu>
2024-10-09 14:31:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ISW2 PROJECT ACUME FORMATTER
+
+Once the results from ACUME has been obtained:
+1. put all the result files .csv in the root directory,
+    that's to say "ISW2_acume_formatter"
+2. in main.py update the project_list variable with
+    the list of result files imported in the root directory
+3. run main.py: you will obtain the same files with "_final"
+    appended in their name
---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+# Function to compute the new columns
+def extract_classifier(s):
+    parts = s.split('_')
+    return parts[1] if len(parts) > 2 else ''
+
+
+def extract_feature_selection(s):
+    parts = s.split('_')
+    return parts[2] if len(parts) > 3 else ''
+
+
+def extract_balancing(s):
+    parts = s.split('_')
+    return parts[3] if len(parts) > 4 else ''
+
+
+def extract_cost_sensitiveness(s):
+    parts = s.split('_')
+    return parts[4] if len(parts) > 5 else ''
+
+
+# Read the CSV file into a DataFrame
+project_list = {'acume_avro', 'acume_bookkeeper'}
+for project in project_list:
+    df = pd.read_csv(project + '.csv')
+
+    # Compute the lengths new columns
+    df['Classifier'] = df['Filename'].apply(extract_classifier)
+    df['Feature_Selection'] = df['Filename'].apply(extract_feature_selection)
+    df['Balancing'] = df['Filename'].apply(extract_balancing)
+    df['Cost_Sensitive'] = df['Filename'].apply(extract_cost_sensitiveness)
+
+    # Insert the new columns
+    cols = df.columns.tolist()
+    insert_pos = cols.index('Filename') + 1  # position to insert after 'A'
+    cols.insert(insert_pos, 'Classifier')
+    cols.insert(insert_pos + 1, 'Feature_Selection')
+    cols.insert(insert_pos + 2, 'Balancing')
+    cols.insert(insert_pos + 3, 'Cost_Sensitive')
+
+    # Reorder the DataFrame columns
+    df = df[cols]
+
+    # Save the updated DataFrame back to a CSV file
+    df.to_csv(project + '_final.csv', index=False)
+
+    print(df)
